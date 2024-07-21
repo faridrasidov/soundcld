8c86940c781cef69867f769ed41fd2e7eb6d91c1
FaridRasidov <ftm5pv70@duck.com>
2024-07-21 18:23:19 +0330

    added "__init__" soundcld pakacge

---
+++ soundcld/__init__.py
@@ -0,0 +1,7 @@
+import soundcld.resource.playlist_album
+import soundcld.resource.track
+import soundcld.resource.user
+import soundcld.resource.webprofiles
+import soundcld.resource.visual
+
+__version__ = '1.0'
---
+++ soundcld/data_types.py
@@ -0,0 +1,8 @@
+"""
+Data Types
+"""
+from soundcld.resource.user import User, BasicUser, MissingUser
+from soundcld.resource.track import Track, BasicTrack, MiniTrack
+from soundcld.resource.playlist_album import AlbumPlaylist, BasicAlbumPlaylist
+from soundcld.resource.webprofiles import WebProfile
+from soundcld.resource.aliases import SearchItem
