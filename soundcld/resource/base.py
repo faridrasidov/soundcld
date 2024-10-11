a2b58080366c950c4f629c4a714f51051a80a34f
FaridRasidov <ftm5pv70@duck.com>
2024-10-11 19:22:55 +0330

    Edit: Now All Objects Are Iterable

--- soundcld/resource/base.py
+++ soundcld/resource/base.py
@@ -1,7 +1,7 @@
 """
 Base Object For SoundCloud
 """
-from dataclasses import dataclass
+from dataclasses import dataclass, fields, is_dataclass
 from datetime import datetime
 from typing import Optional
 
@@ -27,10 +27,47 @@ class BaseData:
         return from_dict(cls, d, cls.dacite_config)
 
     def __getitem__(self, item):
+        """
+        Return Value By Key
+        """
         try:
             return getattr(self, item)
         except AttributeError:
             raise KeyError(f"Key '{item}' not found.")
+
+    def __setitem__(self, item, value):
+        """
+        Set Value To Key
+        """
+        setattr(self, item, value)
+
+    def items(self):
+        """
+        Return a generator of (field_name, value) tuples,
+        converting dataclasses to dicts and datetime to ISO format.
+        """
+        return ((field.name, self._convert_to_dict(getattr(self, field.name))) for field in fields(self))
+
+    def _convert_to_dict(self, value):
+        """
+        Helper method to convert nested
+        dataclass objects to dictionaries recursively.
+        Also handles datetime serialization (to ISO format).
+        """
+        if isinstance(value, datetime):
+            return value.isoformat()
+        elif is_dataclass(value):
+            return {f.name: self._convert_to_dict(getattr(value, f.name)) for f in fields(value)}
+        elif isinstance(value, (list, tuple)):
+            return [self._convert_to_dict(v) for v in value]
+        else:
+            return value
+
+    def __iter__(self):
+        """
+        Return an iterator over the field names.
+        """
+        return (field.name for field in fields(self))
 
 
 @dataclass
